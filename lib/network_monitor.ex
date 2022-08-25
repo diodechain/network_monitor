cf1fc9276220b12386ac533406d466ae95f3ddf4
Dominic Letz <dominic@diode.io>
2022-08-25 11:35:05 +0200

    Windows fixup

--- lib/network_monitor.ex
+++ lib/network_monitor.ex
@@ -187,25 +187,7 @@ defmodule NetworkMonitor do
     try do
       :net.getifaddrs()
     rescue
-      _ ->
-        with {:win32, :nt} <- :os.type(),
-             {net, 0} <- System.cmd("ipconfig", []) do
-          String.split(net, "\r\n")
-          |> Enum.filter(fn str -> String.contains?(str, "IPv4") end)
-          |> Enum.map(fn str ->
-            case Regex.run(~r/([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})/, str) do
-              [_ip, a, b, c, d] ->
-                Enum.map([a, b, c, d], &String.to_integer/1)
-                |> List.to_tuple()
-
-              _ ->
-                nil
-            end
-          end)
-          |> Enum.filter(fn ip -> ip != nil end)
-        else
-          _ -> []
-        end
+      _ -> windows_fallback_getifaddrs()
     end
     |> case do
       {:ok, ifs} -> ifs
@@ -214,5 +196,30 @@ defmodule NetworkMonitor do
     |> Enum.filter(fn %{flags: flags} -> Enum.member?(flags, :up) end)
     |> Enum.map(fn %{addr: %{addr: addr}} -> addr end)
     |> MapSet.new()
+  end
+
+  defp windows_fallback_getifaddrs() do
+    with {:win32, :nt} <- :os.type(),
+         {net, 0} <- System.cmd("ipconfig", []) do
+      ifs =
+        String.split(net, "\r\n")
+        |> Enum.filter(fn str -> String.contains?(str, "IPv4") end)
+        |> Enum.map(fn str ->
+          case Regex.run(~r/([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})/, str) do
+            [_ip, a, b, c, d] ->
+              addr =
+                Enum.map([a, b, c, d], &String.to_integer/1)
+                |> List.to_tuple()
+
+              %{addr: %{addr: addr}, flags: [:up]}
+
+            _other ->
+              nil
+          end
+        end)
+        |> Enum.filter(fn ip -> ip != nil end)
+
+      {:ok, ifs}
+    end
   end
 end
